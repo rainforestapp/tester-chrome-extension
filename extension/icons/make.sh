67e1d09c327e603a1b45d84d7579e6fb7f4b5c99
Emanuel Evans <mail@emanuel.industries>
2016-07-26 18:24:25 -0700

    Externalize javascript

--- extension/icons/make.sh
+++ extension/icons/make.sh
@@ -1,7 +1,15 @@
+#!/usr/bin/env bash
 mogrify original.png -trim +repage -fuzz 1%
 convert original.png -thumbnail 128x128 icon128.png
 convert original.png -thumbnail 48x48 icon48.png
 convert original.png -thumbnail 38x38 icon38.png
 convert original.png -thumbnail 19x19 icon19.png
 convert original.png -thumbnail 16x16 icon16.png
+
+convert original.png -colorspace Gray -thumbnail 128x128 icon128_grey.png
+convert original.png -colorspace Gray -thumbnail 48x48 icon48_grey.png
+convert original.png -colorspace Gray -thumbnail 38x38 icon38_grey.png
+convert original.png -colorspace Gray -thumbnail 19x19 icon19_grey.png
+convert original.png -colorspace Gray -thumbnail 16x16 icon16_grey.png
+
 optipng *.png
--- src/constants.js
+++
@@ -1,9 +0,0 @@
-export const RED = [255, 0, 0, 230];
-export const GREEN = [0, 255, 0, 230];
-export const GREY = [0, 0, 0, 230];
-export const BASE_URL = 'https://portal.rainforestqa.com';
-export const WORK_AVAILABLE_URL = `${BASE_URL}/api/1/testers/`;
-export const DEFAULT_INTERVAL = 25 * 1000;
-export const RAVEN_URL = 'https://a7b0c76390cc47208e38b884fd60ff3d@app.getsentry.com/68477';
-export const IDLE_TIME = 3 * 60; // 3 min.
-export const SHUTOFF_TIME = 5 * 60 * 1000; // 5 min.
